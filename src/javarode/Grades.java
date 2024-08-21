4b8d724cf220dd72d63560937561b391cc05e2fd
rodelizalarosa <rodeliza.2002@gmail.com>
2024-08-21 14:41:49 +0800

    updated grades and product

---
+++ src/javarode/Grades.java
@@ -0,0 +1,33 @@
+package javarode;
+
+public class Grades {
+    
+    int id;
+    String name;
+    double pr, m, p, f;
+    
+    public void addGrades (int sid, String name, double prlm, double mt, double pf, double fnl) {
+    
+        this.id = sid;
+        this.name = name;
+        this.pr = prlm;
+        this.m = mt;
+        this.p = pf;
+        this.f = fnl;
+    
+    }
+    
+    public void viewGrades (){
+        
+        double avg = (this.pr + this.m + this.p + this.f) / 4;
+        String remarks = (avg < 3.0) ? "Passed" : "Failed";
+        
+        //Display
+        
+        System.out.printf("%-10d %-10s %-10.2f %-10.2f %-10.2f %-10.2f %-10.2f %-10s\n",
+                           this.id, this.name, this.pr, this.m, this.p, this.f, avg, remarks );
+        
+        
+    }
+    
+}
--- src/javarode/JavaRode.java
+++ src/javarode/JavaRode.java
@@ -8,9 +8,24 @@ public class JavaRode {
     
     public static void main(String[] args) {
        
-       netPayAct npa = new netPayAct();
-       npa.getPay();
+//       netPayAct npa = new netPayAct();
+//       npa.getPay();
+        
+//        Grades gr = new Grades();
+//        gr.addGrades (11001, "Rodeliza", 1.0, 1.0, 1.0, 1.0);
+//        gr.viewGrades();
+//        
+//        Grades gr1 = new Grades();
+//        gr.addGrades (11001, "Myca", 1.0, 1.0, 1.0, 1.0);
+//        gr.viewGrades();
     
+           Products pr = new Products();
+           pr.addProduct (10011, "Soap", 30, 30, 20.00 );
+           pr.viewProduct();
+           
+           Products pr1 = new Products();
+           pr.addProduct (10011, "Cards", 0, 60, 50.00 );
+           pr.viewProduct();
+           
     }
-    
 }
